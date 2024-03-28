cc43da5cd6d21d8b351eeff228735cba40eefb25
omooooori <omooooori432@gmail.com>
2024-03-29 08:29:53 +0900

    Crate chat portfolio project (SwiftUI)

---
+++ ChatPortfollio/ChatPortfollio/ChatPortfollioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPortfollioApp.swift
+//  ChatPortfollio
+//
+//  Created by ryosshim on 2024/03/29.
+//
+
+import SwiftUI
+
+@main
+struct ChatPortfollioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
